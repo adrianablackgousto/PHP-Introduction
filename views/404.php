b09b2dad60640711c5ddcd1fa073b50c95cc486d
Adriana Black <adriana.black@gousto.co.uk>
2024-05-17 14:13:40 +0100

    refactored routing

---
+++ views/404.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+
+<body class="h-full">
+
+<div class="min-h-full">
+
+    <?php require('partials/nav.php'); ?>
+
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            SORRY NOT FOUND
+            <a href="/">Go back home</a>
+        </div>
+    </main>
+    <?php require('partials/footer.php'); ?>
+</div>
+
+
+</body>
+</html>
+
